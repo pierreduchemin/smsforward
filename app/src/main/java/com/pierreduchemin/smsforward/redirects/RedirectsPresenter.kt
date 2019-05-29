15fbb2d6a547f9fb4cd6d51b1fd8ec5a6ff18129
Pierre Duchemin <mail@pierre-duchemin.fr>
2019-05-29 17:34:06 -0400

    feat: extract string resources

--- app/src/main/java/com/pierreduchemin/smsforward/redirects/RedirectsPresenter.kt
+++ app/src/main/java/com/pierreduchemin/smsforward/redirects/RedirectsPresenter.kt
@@ -14,6 +14,8 @@ class RedirectsPresenter(
     private val view: RedirectsContract.View
 ) : RedirectsContract.Presenter {
 
+    private val tag: String = RedirectsActivity::class.java.simpleName
+
     private lateinit var smsVerifyCatcher: SmsVerifyCatcher
 
     init {
@@ -26,38 +28,51 @@ class RedirectsPresenter(
 
     override fun setRedirect(source: String, destination: String) {
         if (source.isEmpty()) {
-            view.showError(R.string.redirects_error_rempty_source)
+            view.showError(R.string.redirects_error_empty_source)
             return
         }
         if (destination.isEmpty()) {
-            view.showError(R.string.redirects_error_rempty_destination)
+            view.showError(R.string.redirects_error_empty_destination)
             return
         }
         if (source == destination) {
             view.showError(R.string.redirects_error_source_and_redirection_must_be_different)
             return
         }
+        if (!isValidNumber(source)) {
+            view.showError(R.string.redirects_error_invalid_source)
+            return
+        }
+        if (!isValidNumber(destination)) {
+            view.showError(R.string.redirects_error_invalid_destination)
+            return
+        }
         if (!view.hasPermission(Manifest.permission.SEND_SMS)) {
             view.askPermission(Manifest.permission.SEND_SMS)
             return
         }
+
+        // in service
         smsVerifyCatcher = SmsVerifyCatcher(activity, OnSmsCatchListener { message ->
-            Log.d(RedirectsActivity::class.java.simpleName, "Caught a SMS from $source: $message")
-            sendSMS(destination, "SMS received from $source: $message")
+            Log.i(tag, "Caught a SMS from $source: $message")
+            sendSMS(destination, activity.getString(R.string.redirects_info_sms_received_from, source, message))
         })
+        smsVerifyCatcher.onStart()
+
         view.redirectSetConfirmation(source, destination)
-        onStart()
+    }
+
+    private fun isValidNumber(phoneNumber: String): Boolean {
+        return phoneNumber.length >= 4 || phoneNumber.length <= 16
     }
 
     private fun sendSMS(phoneNumber: String, message: String) {
-        Log.d(RedirectsActivity::class.java.simpleName, "Forwarding to $phoneNumber: $message")
+        Log.i(tag, "Forwarding to $phoneNumber: $message")
         val smsManager = SmsManager.getDefault()
         smsManager.sendTextMessage(phoneNumber, null, message, null, null)
     }
 
     override fun onStart() {
-        if (::smsVerifyCatcher.isInitialized)
-            smsVerifyCatcher.onStart()
     }
 
     override fun onStop() {
