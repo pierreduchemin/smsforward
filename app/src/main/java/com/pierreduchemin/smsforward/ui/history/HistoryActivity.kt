0864855c943cd0719dad37baf08de5188f721d37
Pierre Duchemin <12597-pierreduchemin@users.noreply.gitlab.com>
2021-07-14 15:39:19 -0400

    feat: add redirection history

---
+++ app/src/main/java/com/pierreduchemin/smsforward/ui/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.pierreduchemin.smsforward.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pierreduchemin.smsforward.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
